e3ecfefb17828e958af553778fa3ac15e5c5b605
navdeep263-wq <navdeepkaur83882@gmail.com>
2026-05-16 22:52:30 +0530

    Practice Array

---
+++ Arrays/eachElementfrequencyCount.java
@@ -0,0 +1,22 @@
+public class eachElementfrequencyCount {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,3,4,5,4,6,7,5};
+        boolean[] visited = new boolean[arr.length];
+
+        for(int i = 0; i < arr.length; i++){
+            if(visited[i] == true){
+                continue;
+            }
+            int count = 1;
+
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                    visited[j] = true;
+                }
+            }
+            System.out.println(arr[i] + " = " + count);
+        }
+         
+    }
+}
---
+++ Arrays/reversePrint.java
@@ -0,0 +1,9 @@
+public class reversePrint {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        //print last index to first index
+       for(int i = arr.length - 1; i >= 0; i--){
+        System.out.println(arr[i] + " ");
+       }
+    }
+}
